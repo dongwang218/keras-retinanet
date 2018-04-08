82ebf8cab497e920cd512ff9512e2c870c0303cc
Dong <dwoanngg@gmail.com>
2018-04-08 23:12:07 +0000

    include empty file in dlib xml to csv

--- examples/dlib_xml_to_ssd_csv.py
+++ examples/dlib_xml_to_ssd_csv.py
@@ -8,9 +8,12 @@ with open(sys.argv[1], 'r') as f:
   soup = BeautifulSoup(f, 'xml')
   for img in soup.findAll('image'):
     file = img['file']
-    for box in img.findAll('box'):
+    boxes = img.findAll('box')
+    for box in boxes:
       xmin = box['left']
       xmax = int(box['left']) + int(box['width'])
       ymin = box['top']
       ymax = int(box['top']) + int(box['height'])
       csv.write('%s,%s,%s,%s,%s,1\n' % (os.path.basename(file), xmin, xmax, ymin, ymax))
+    if len(boxes) == 0:
+      csv.write('%s,,,,,\n' % (os.path.basename(file)))
